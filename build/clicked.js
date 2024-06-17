42db597065c8dc30c40ab1fa71ab430956f68563
Mohsin Tabba Ovais <ahme.mohsin@gmail.com>
2024-06-18 01:22:36 +0500

    Clickable movie card

---
+++ build/clicked.js
@@ -0,0 +1,45 @@
+
+let data = localStorage.getItem("movieClicked");
+// if(data==""){return}
+if(data){
+    data= JSON.parse(data)
+    console.log(data.poster_path, data.title,data.vote_average,data.popularity, data.overview)
+    appendHTML2( data.poster_path, data.title,data.vote_average,data.popularity,data.overview)
+}
+
+
+//--------------- Clickable Movie card directed towards new html file-----------------
+
+function appendHTML2(poster,title,rating,popularity,overview,cast){
+    const appendHere = document.getElementById('pg1-newhtml-item1');
+    const newDiv = document.createElement('div');
+    newDiv.className="movieDetail"
+    newDiv.innerHTML =  
+    `
+    <div class= "mainDivNewhtml">
+        <div class="imgStyleNewhtml">
+        <img src="https://image.tmdb.org/t/p/original${poster}" alt="">
+        </div>
+        <div class="movieplotNewhtml">
+            <div class="titleMovie">
+                <h1>${title}</h1>
+            </div>
+            <div class="ratingMovie">
+                <h4>Rating: ${rating}</h4> 
+                <h4>Popularity: ${popularity}</h4> 
+            </div>
+            <div class="overviewMovie">
+                <p>${overview}</p>
+            </div>
+            <div class="castMovie">
+                <h4>Cast: ${cast}</h4> 
+            </div>
+            <div class="backMain">
+                <a href="../index.html">Back to main</a>
+            </div>
+        </div>
+    </div>
+    <br>
+    `;
+    appendHere.append(newDiv);
+  }
--- build/js-testproject.js
+++ build/js-testproject.js
@@ -1,7 +1,9 @@
 
+// ------------- Global Variables & Constants ------------------------------------
 const apiKey = "4aeff827ceeeb2ab2e0b87dbe7c509da";
 
 
+//------------ Search button event and fetch api function--------------------------
 document.getElementById("searchBtn").addEventListener('click', function() {
   clear();
   let searchTerm = document.getElementById("mvname").value.toLowerCase();
@@ -9,26 +11,27 @@ document.getElementById("searchBtn").addEventListener('click', function() {
   searchTerm=searchTerm.replaceAll(" ","+",)
 
   let data = localStorage.getItem(searchTerm);
+//-------------- If available in local storage -----------------------------------
   if(data){
     console.log("Is in local storage")
     data= JSON.parse(data)
     data.length==0 ? document.getElementById("appendHere").innerHTML="No Movie Found" :document.getElementById("appendHere").innerHTML="";
     for(let i=0; i<data.length; i++){
-      appendHTML( data[i].poster_path,data[i].title, data[i].release_date)
-      // console.log(data.recordings[i].title)
+      appendHTML1
+    ( data[i].poster_path,data[i].title, data[i].release_date,i)
+
     }
   }
+// -------------- Else Search from api ------------------------------------------------
   else{
     console.log("Not in Local Storage")
     fetch(`https://api.themoviedb.org/3/search/movie?query=${searchTerm}&api_key=${apiKey}`)
     .then(response => response.json())
     .then(data=> {
-      // console.log(data.results)
-      // localStorage.setItem("movieName", searchTerm)
       localStorage.setItem(searchTerm, JSON.stringify(data.results))
       for(let i=0; i<data.results.length; i++){
-        appendHTML( data.results[i].poster_path,data.results[i].title,data.results[i].release_date)
-        // console.log(data.recordings[i].title)
+        appendHTML1
+        (data.results[i].poster_path,data.results[i].title,data.results[i].release_date,i)
       }
     })
     .catch(error => {
@@ -39,6 +42,7 @@ document.getElementById("searchBtn").addEventListener('click', function() {
   }
 });
   
+// ------------------ Capturing Enter button on search ---------------------
 document.getElementById("mvname").addEventListener('keydown', function(event) {
   if (event.key == "Enter") {
     clear();
@@ -46,21 +50,32 @@ document.getElementById("mvname").addEventListener('keydown', function(event) {
   }
 });
 
+//-------------- Function to clear the searched movie -------------------------
+
 function clear() {
   document.getElementById("appendHere").innerHTML = "";
 }
 
+function clicked(id){
+  let searchTerm = document.getElementById("mvname").value.toLowerCase();
+  if(searchTerm==""){return}
+  searchTerm=searchTerm.replaceAll(" ","+",)
+  let data = localStorage.getItem(searchTerm);
+  data= JSON.parse(data)
+  localStorage.setItem('movieClicked', JSON.stringify(data[id]))
+  
+  window.location.href= "/build/movieCard.html"  
+}
 
-
-function appendHTML(poster,mvTitle,year){
+//------------- Function to pass append movie card divs ---------------------
+function appendHTML1(poster,mvTitle,year,id){
   const appendHere = document.getElementById('appendHere');
   const newDiv = document.createElement('div');
-  newDiv.className="movieCard"
   newDiv.className="movieCard"
   newDiv.innerHTML =  
   `
   <div class="imgStyle">
-  <img src="https://image.tmdb.org/t/p/original${poster}" alt="">
+  <img src="https://image.tmdb.org/t/p/original${poster}" alt="" onclick="clicked(${id})">
   </div>
   <br>
   <div class="yearStyle">
@@ -72,3 +87,4 @@ function appendHTML(poster,mvTitle,year){
   `;
   appendHere.append(newDiv);
 }
+
